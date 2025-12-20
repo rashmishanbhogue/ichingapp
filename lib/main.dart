389620e511e297b9ebaed484734b83441b6fae5e
rashmishanbhogue <rashmi.shanbhogue@gmail.com>
2025-12-20 18:56:20 +0530

    splash update
    logo update
    app icon update
    custom splash screen removal - default android splash updated to reflect the required

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // main.dart, start of the application
-import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'personal_details.dart';
 import './dart/theme.dart';
 
@@ -16,51 +15,7 @@ class IchingApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.theme,
-      home: const SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  SplashScreenState createState() => SplashScreenState();
-}
-
-class SplashScreenState extends State<SplashScreen> {
-  late Timer navigationTimer; // avoid potential memory leak
-
-  @override
-  void initState() {
-    super.initState();
-    navigationTimer = Timer(const Duration(milliseconds: 500), () {
-      if (mounted) {
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => const PersonalDetails()),
-        );
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    navigationTimer.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppTheme.splashBackgroundColor,
-      body: Center(
-        child: SizedBox(
-          width: 160,
-          height: 224,
-          child: SvgPicture.asset(
-              'assets/images/splash.svg'), // moved the svg code to an imported asset file
-        ),
-      ),
+      home: const PersonalDetails(),
     );
   }
 }
